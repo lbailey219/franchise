ccb4bb294664cdec12e9580203ab923e77281c72
Lewis Bailey <lbaileyjr219@gmail.com>
2019-03-14 21:16:52 -0400

    zipcode function

---
+++ script.js
@@ -0,0 +1,13 @@
+function getZipcode(zipcode) {
+    console.log(zipcode);
+}
+
+
+$(document).ready(function() {
+// evnt handlers
+  $('#go-btn').click(function(){
+    var zipcode = $('#zipcode').val();
+
+    getZipcode('zipcode');
+  });
+});
