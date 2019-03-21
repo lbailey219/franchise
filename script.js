46285b9b82536a16fdf279c5358826bee211d4bd
Lewis Bailey <lbaileyjr219@gmail.com>
2019-03-21 10:37:32 -0400

    output form incomplete

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 function getZipcode(zipcode) {
-    console.log(zipcode);
+  // debugger
+  console.log(zipcode);
+  $('#modal--zipcode').val(zipcode);
 }
 
-
 $(document).ready(function() {
-// evnt handlers
+  // evnt handlers
   $('#go-btn').click(function(){
     var zipcode = $('#zipcode').val();
 
     getZipcode(zipcode);
+    });
+    $('#modal--submit').on('click',function(){
+       $('#removal-result').slideToggle();
+     });
+
+
+
+
   });
-});
