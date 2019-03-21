79f8d4022f944cf45424751d8095b828cf3148b6
Alan Mabry <alan.mabry@gmail.com>
2019-03-21 13:05:59 -0400

    fixed some broken markup, added formSubmit method & gitignore file

--- script.js
+++ script.js
@@ -4,21 +4,24 @@ function getZipcode(zipcode) {
   $('#modal--zipcode').val(zipcode);
 }
 
+function submitForm(zipcode, email) {
+  console.log('this is the zipcode: ', zipcode);
+  console.log(email);
+  // $('#modal--zipcode').val(zipcode);
+}
+
 $(document).ready(function() {
   // evnt handlers
   $('#go-btn').click(function(){
     var zipcode = $('#zipcode').val();
 
     getZipcode(zipcode);
-    });
-    
-    $('#modal--submit').click(function(){
-    var zipcode = $('#zipcode').val();
-    var email = $('[name="email"]').val();
-
-     submitForm('zipcode, email');
-   });
-
-
-
   });
+
+  $('#modal--submit').click(function(){
+    var zipcode = $('#modal--zipcode').val();
+    var email = $('#modal--email').val();
+
+    submitForm(zipcode, email);
+  });
+});
